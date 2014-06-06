4e590a3bd662aebd99a4dfb78a2eca7b89b0747a
Cendrb <cendrb@gmail.com>
2014-06-06 14:09:17 +0200

    First commit :)

---
+++ Kýp Sajlink/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using VPN_Penis_Sender;
+
+namespace Kýp_Sajlink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("192.168.1.4", 6969);
+            client.Client.Send(Helpers.GetBytes("pierdole.mp3", sizeof(char) * 128));
+        }
+    }
+}
---
+++ MD5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.IO;
+
+namespace MD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream("penis", FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite);
+            using(System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] penis = md5.ComputeHash(file);
+                Console.ReadKey();
+            }
+        }
+    }
+}
